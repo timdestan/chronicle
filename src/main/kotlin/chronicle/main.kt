3a0ceaa193569c4762197e733ef63a33a75ba401
timdestan <tim.destan@gmail.com>
2015-03-22 22:58:45 -0400

    Adding Artist and Track classes

--- src/main/kotlin/chronicle/main.kt
+++ src/main/kotlin/chronicle/main.kt
@@ -1,5 +1,14 @@
 package chronicle
 
+import java.util.UUID;
+
 fun main(args: Array<String>) {
-  println("こんにちは世界")
+  val weezer =
+      Artist("Weezer",
+             UUID.fromString("6fe07aa5-fec0-4eca-a456-f29bff451b04"))
+  val elScorcho =
+      Track("El Scorcho",
+            weezer,
+            UUID.fromString("58d5ee31-a92c-347c-89d6-3acc765cab9b"))
+  println(elScorcho)
 }
