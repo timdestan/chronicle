a0f6f22bd2a0388f6dcec823442536ab2053ccd0
timdestan <tim.destan@gmail.com>
2015-03-26 19:13:00 -0400

    Getting top tracks for a user
    Got rid of script because running FSI locks up the DLLs, forcing a
    restart of VS, making it rather pointless for "quick" feedback.

--- chronicle.tests/LastFmImporterTests.fs
+++ chronicle.tests/LastFmImporterTests.fs
@@ -11,3 +11,8 @@ let ``Can read a user`` () =
     let user = LastFmImporter.getUser "tj6186" |> Async.RunSynchronously
     user.Name |> should equal "tj6186"
     user.TrackCount |> should be (greaterThan 26000)
+
+[<Test>]
+let ``Can read tracks for a user`` () =
+    let tracks = LastFmImporter.getTracksForUser "tj6186" |> Async.RunSynchronously |> Seq.toArray
+    tracks.Length |> should be (greaterThan 10)
--- chronicle/LastFmImporter.fs
+++ chronicle/LastFmImporter.fs
@@ -3,6 +3,7 @@
 open System
 open FSharp.Data
 open HttpClient
+open Microsoft.FSharp.Control
 
 // Build our types based on samples of the JSON we expect.
 type LastFmApiCredentials = JsonProvider<"data/lastfm/credentials.sample.json">
@@ -15,14 +16,20 @@ let loadApiKey () =
         __SOURCE_DIRECTORY__ + "/data/lastfm/credentials.json").ApiKey
 
 type User(name: string, trackCount: int) =
-    member this.Name = name
-    member this.TrackCount = trackCount
+    member m.Name = name
+    member m.TrackCount = trackCount
 
-type Artist(name: string, 
-            mbid: Guid option) = class end
+type Artist(name: string,
+            mbid: Guid option) =
+    member m.Name = name
+    member m.Mbid = mbid
+
 type Track(name: string,
-           artist: Artist, 
-           mbid: Guid option) = class end
+           artist: Artist,
+           mbid: Guid option) =
+    member m.Name = name
+    member m.Artist = artist
+    member m.Mbid = mbid
 
 let baseUri = "http://ws.audioscrobbler.com/2.0/"
 
@@ -34,6 +41,15 @@ let buildUserInfoRequest(username: string, apiKey: string) =
     |> withQueryStringItem { name="method"; value="user.getInfo" }  
     |> withQueryStringItem { name="user"; value=username }  
 
+let buildGetTracksRequest(username: string, apiKey: string) =
+    createRequest Get baseUri
+    |> withQueryStringItem { name="api_key"; value=apiKey }  
+    |> withQueryStringItem { name="format"; value="json" }  
+    |> withQueryStringItem { name="method"; value="user.getTopTracks" }  
+    |> withQueryStringItem { name="user"; value=username }
+    |> withQueryStringItem { name="page"; value="1" }
+
+
 // Gets some user information from the Last.fm API
 let getUser(username: string):Async<User> = async {
     let apiKey = loadApiKey ()
@@ -41,4 +57,24 @@ let getUser(username: string):Async<User> = async {
     let! response = (request |> getResponseBodyAsync)
     let json = LastFmUser.Parse(response)
     return User(json.User.Name, json.User.Playcount)
+}
+
+let parseArtist(artist: LastFmTracks.Artist) = 
+    new Artist(artist.Name, artist.Mbid)
+
+// Parse the JSON to a tracks object
+let parseTracks(tracks: LastFmTracks.Root) = seq {    
+    for track in tracks.Toptracks.Track do
+        let artist = parseArtist(track.Artist)
+        yield new Track(track.Name, artist, track.Mbid)
+}
+
+// Gets all the user's tracks
+let getTracksForUser(username: String) = async {
+    let apiKey = loadApiKey ()
+    let! user = getUser(username)
+    let request = buildGetTracksRequest(username, apiKey)
+    let! response = (request |> getResponseBodyAsync)
+    let json = LastFmTracks.Parse(response)
+    return parseTracks(json)
 }
