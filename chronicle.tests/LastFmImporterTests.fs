c35f0eb3ff404bb10645ff53ccac6d92dc13d8c4
timdestan <tim.destan@gmail.com>
2015-03-26 18:57:47 -0400

    Adding test on the importer

---
+++ chronicle.tests/LastFmImporterTests.fs
@@ -0,0 +1,13 @@
+﻿module LastFmImporterTests
+
+open LastFmImporter
+
+open NUnit.Framework
+open FsUnit
+open Microsoft.FSharp.Control
+
+[<Test>]
+let ``Can read a user`` () =
+    let user = LastFmImporter.getUser "tj6186" |> Async.RunSynchronously
+    user.Name |> should equal "tj6186"
+    user.TrackCount |> should be (greaterThan 26000)
